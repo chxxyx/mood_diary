cbf6b7904193a36e2836ca93c1a082447ca50d59
{yklee} <lyk1040@gmail.com>
2023-07-20 01:40:45 +0900

    todolist js

--- todo.js
+++ todo.js
@@ -1,20 +1,46 @@
-let input = document.getElementById("pw");
-input.addEventListener("keyup", (e)=>{
-    if(e.key == "Enter") {
-        alert("로그인")
+//유저가 값을 입력한다
+//enter를 치면 할일이 추가된다
+//complet를 누르면 완료하고 밑줄
+//active 
+//
+
+let todoInput = document.getElementById("todo-input");
+let addButton = document.getElementById("complete");
+let todoList = []
+addButton.addEventListener("click",addTask);
+
+function addTask(){
+    
+    let task = {
+        todoContent : todoInput.value,
+        isComplete:false
     }
-})
-
-let toDo = [];
-let id = 0;
-
-
-const init = () => {
-    toDoInputElem.addEventListener('keypress', (e) =>{
-        if( e.key === 'Enter' ){
-            appendtoDo(e.target.value); toDoInputElem.value ='';
-        }
-    })
+    todoList.push(task)
+    console.log(todoList);
+    render()
 }
 
-init()
+function render(){
+    let resultHTML = ''
+    for(let i=0; i<todoList.length; i++){
+        resultHTML += `<div id="text-box">
+        <ul class="todo-list">
+        
+           <div class="text">
+               <div>${todoList[i].todoContent}</div>
+               
+                <div>
+                    <button>completed</button>
+                    <button>no</button>
+                </div>
+            </div>
+           </ul>
+
+       </div>`
+       
+    }
+
+    
+    document.getElementById("text-box").innerHTML=resultHTML
+    
+}
