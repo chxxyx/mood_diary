b2f843105e2d719985598ec9daba32326aec03bd
k-minyoung <alsdudsk12@naver.com>
2023-07-21 21:04:26 +0900

    swipe test2

--- kmy/board.js
+++ kmy/board.js
@@ -4,6 +4,12 @@ console.log(date);
 let year = date.getFullYear()
 let month = date.getMonth()+1
 let day = date.getDate()-1
-let date2 = (`${year}-${month >=10 ? month : "0"+month}-${day >=10 ? day :"0"+day}`)
+let date2 = (`${month >=10 ? month : +month}.${day >=10 ? day :"0"+day}`)
 document.getElementById("date2").innerHTML=date2
 
+function heart(){
+    $(".heart").attr("class", "fa-solid fa-heart redHeart")
+}
+function heart2(){
+    $(".heart").attr("class", "fa-regular fa-heart heart")
+}
