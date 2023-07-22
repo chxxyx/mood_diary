32f3aa859b3ce76533314a8a7e591d756d084b20
k-minyoung <alsdudsk12@naver.com>
2023-07-22 19:36:11 +0900

    Feat modal

--- kmy/board.js
+++ kmy/board.js
@@ -1,4 +1,4 @@
-
+// 날짜
 let date = new Date()
 console.log(date);
 let year = date.getFullYear()
@@ -7,19 +7,33 @@ let day = date.getDate()-1
 let date2 = (`${month >=10 ? month : +month}.${day >=10 ? day :"0"+day}`)
 document.getElementById("date2").innerHTML=date2
 
-function heart(){
-    const ht=$('.fa-heart');
+// function heart(){
+//     const ht=$('.fa-heart');
+    
   
-    if(ht.hasClass('redHeart')== true) {
-        $(".redHeart").attr("class", "fa-regular fa-heart heart")
-        console.log('빈하트')
-    } else if (ht.hasClass('heart')==true){
-        $(".heart").attr("class", "fa-solid fa-heart redHeart")
-        console.log('빨간하트')
-    }
+//     if(ht.hasClass('redHeart')== true) {
+//         $(".redHeart").attr("class", "fa-regular fa-heart heart")
+
+//     } else if (ht.hasClass('heart')==true){
+//         $(".heart").attr("class", "fa-solid fa-heart redHeart")
+        
+//     }
     
    
-}
-function heart2(){
-  
-}
+// }
+
+ // 모든 div 요소에 대해 이벤트 리스너를 등록
+ var hearts = document.querySelectorAll(".fa-heart");
+ hearts.forEach(function(i) {
+     i.addEventListener("click", toggleHeart);
+ });
+
+ // 이 함수는 클릭한 div 요소에 클래스를 토글
+ function toggleHeart(event) {
+     var clickedhearts = event.currentTarget;
+     clickedhearts.classList.toggle("fa-regular");
+     clickedhearts.classList.toggle("fa-solid");
+    //  clickedhearts.classList.toggle("fa-heart");
+     clickedhearts.classList.toggle("heart");
+     clickedhearts.classList.toggle("redHeart");
+ }
