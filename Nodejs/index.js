d529071e102c8bd5665d40dc5bdcd998d17354a1
{yklee} <lyk1040@gmail.com>
2023-07-28 20:51:19 +0900

    node

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -31,7 +31,7 @@ app.get('/',(req,res)=>{
 //get : http 통신을 나타냄
 //send :문자열 데이터를 보낸다 따라서 hello express로 뜸
 //localhost : 8000/ 뒤에 없으면 무시 
-//req res순서 주의 
+//req res순서 주의 zhttps://velog.io/@spy03128/%EB%B0%B1%EC%A4%80-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Github-%EC%9E%90%EB%8F%99%EC%9C%BC%EB%A1%9C-%EC%BB%A4%EB%B0%8B%ED%95%98%EA%B8%B0
 app.get('/kdt9',(req,res)=>{
     res.render('test',{num:[2,3,4,5,6,7,8,9]})
     // res.render('test')
@@ -44,3 +44,5 @@ app.get('/kdt9',(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`);
 })
+
+
